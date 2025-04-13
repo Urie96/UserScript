74df52f3e64cd336e81c229195a275585a31d918
杨锐 <lubui.com@gmail.com>
2025-04-13 21:46:33 +0800

    feat: init

---
+++ TestHideElements.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        New script
+// @namespace   https://github.com/Urie96/UserScript
+// @match       *://example.org/*
+// @version     1.0
+// @author      Urie96
+// @description 2025/04/13 21:43:37
+// @run-at      document-idle
+// @grant       GM_addStyle
+// ==/UserScript==
+
+function hideElement(selector) {
+  GM_addStyle(`${selector} {display: none !important;}`);
+}
+
+hideElement("#onesite-portal-header");
+hideElement("#onesite-framework-root > div > div > div.with-sider-expanded");
