b71ccdc24ed9ff988b840dbbe823ad4419cc8653
yangrui.0 <yangrui.0@bytedance.com>
2025-04-14 10:37:01 +0800

    feat: up

---
+++ DeleteSomeKeymaps.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        DeleteSomeKeymaps
+// @namespace   https://github.com/Urie96/UserScript
+// @match       *://*/*
+// @version     1.0
+// @author      Urie96
+// @description 2025/04/14 10:35:24
+// @license     MIT
+// @run-at      document-start
+// @grant       none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  document.addEventListener(
+    "keydown",
+    function (e) {
+      // 例如要移除Ctrl+S快捷键
+      if (e.metaKey && (e.key === "k" || e.key === "j" || e.key === "l")) {
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        return false;
+      }
+    },
+    true,
+  );
+})();
